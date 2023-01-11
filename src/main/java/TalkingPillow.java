351a99d8106f7a992f2586bc6066afe42a0c31dc
Alex <bardovski.alesha@mail.ru>
2023-01-11 19:00:13 +0300

    feat: add application.properties

--- src/main/java/TalkingPillow.java
+++ src/main/java/TalkingPillow.java
@@ -1,18 +1,19 @@
 import Listeners.MassageListener;
-import busineslogic.GameManager;
-import busineslogic.IGameManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.apache.commons.configuration.PropertiesConfiguration;
 
 public class TalkingPillow {
-    private static final String BOT_TOKEN = "MTA1NzM3ODQzNTg3OTY3ODAyMw.GGvTVf.t5oawXwiXmXSpJ7w73KrYsAsiOUbNeB8Xbsb2w";
-    private static final IGameManager gameManager = GameManager.getInstance();
+    private static final PropertiesConfiguration config = new PropertiesConfiguration();
     public static void main(String[] arguments) throws Exception
     {
-        JDA api = JDABuilder.create(BOT_TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_BANS,
+        config.load("application.properties");
+        String botToken = config.getString("bot_token");
+        JDA api = JDABuilder.create(botToken, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_BANS,
                         GatewayIntent.SCHEDULED_EVENTS)
                 .addEventListeners(new MassageListener())
                 .build();
+
     }
 }
