d63c730a55324a303b7d74fef27c511574644e8a
Alex <bardovski.alesha@mail.ru>
2023-05-04 11:04:34 +0300

    feat: added logging to MessageListener.java and TalkingPillow.java

--- src/main/java/Listeners/MessageListener.java
+++ src/main/java/Listeners/MessageListener.java
@@ -9,9 +9,12 @@ import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class MassageListener  extends ListenerAdapter {
+public class MessageListener extends ListenerAdapter {
     private final IGameFacade gameFacade = GameFacade.getInstance();
+    private static final Logger logger = LoggerFactory.getLogger(MessageListener.class);
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
@@ -22,46 +25,61 @@ public class MassageListener  extends ListenerAdapter {
         Guild guild = event.getGuild();
         Member member = event.getMember();
         MessageChannel messageChannel = event.getChannel();
+        logger.info("got message \"" + message + "\" from " + member.getNickname() == null ?
+                member.getUser().getName() :member.getNickname());
+
         if (member.getVoiceState().getChannel() == null){
+            logger.info("member " + member.getNickname() == null ?
+                    member.getUser().getName() : member.getNickname() + "is not in voce channel. return");
             messageChannel.sendMessage("Вы должны быть в голосовом канале").queue();
             return;
         }
 
         try {
             if (content.equals("!start")) {
+                logger.info("processing command START for member " + member.getNickname() == null ?
+                        member.getUser().getName() :member.getNickname());
                 AudioChannelUnion audioChannel = member.getVoiceState().getChannel();
                 gameFacade.startGame(guild, audioChannel, event.getChannel(), member);
-                //messageChannel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
             } else if (content.equals("!accept")) {
+                logger.info("processing command ACCEPT for member " + member.getNickname() == null ?
+                        member.getUser().getName() :member.getNickname());
                 AudioChannelUnion audioChannel = member.getVoiceState().getChannel();
                 gameFacade.acceptGame(guild, audioChannel, event.getChannel(), member);
-                //event.getMember().deafen(false).queue();
             } else if (content.equals("!stop")) {
+                logger.info("processing command STOP for member " + member.getNickname() == null ?
+                        member.getUser().getName() :member.getNickname());
                 AudioChannelUnion audioChannel = member.getVoiceState().getChannel();
                 gameFacade.voteForStop(guild, audioChannel, event.getChannel(), member);
-                //event.getMember().deafen(false).queue();
             } else if (content.equals("!resetStop")) {
+                logger.info("processing command RESETSTOP for member " + member.getNickname() == null ?
+                        member.getUser().getName() :member.getNickname());
                 AudioChannelUnion audioChannel = member.getVoiceState().getChannel();
                 gameFacade.resetVoteForStop(guild, audioChannel, event.getChannel(), member);
-                //event.getMember().deafen(false).queue();
             } else if (content.equals("!rec")) {
+                logger.info("processing command REQUEST for member " + member.getNickname() == null ?
+                        member.getUser().getName() :member.getNickname());
                 AudioChannelUnion audioChannel = member.getVoiceState().getChannel();
                 gameFacade.requestPillow(guild, audioChannel, event.getChannel(), member);
-                //event.getMember().deafen(false).queue();
             } else if (content.equals("!resetRec")) {
+                logger.info("processing command RESETREQUEST for member " + member.getNickname() == null ?
+                        member.getUser().getName() :member.getNickname());
                 AudioChannelUnion audioChannel = member.getVoiceState().getChannel();
                 gameFacade.resetRequestPillow(guild, audioChannel, event.getChannel(), member);
-                //event.getMember().deafen(false).queue();
             } else if (content.contains("!acceptRec") && message.getMentions().getMembers().size() == 1) {
                 AudioChannelUnion audioChannel = member.getVoiceState().getChannel();
                 Member toWhom = message.getMentions().getMembers().get(0);
+                logger.info("processing command ASSEPTREQUEST("
+                        + toWhom.getNickname() == null ? toWhom.getUser().getName() :toWhom.getNickname()
+                        + ") for member "
+                        + member.getNickname() == null ? member.getUser().getName() :member.getNickname());
                 gameFacade.acceptPillowRequest(guild, audioChannel, event.getChannel(), member, toWhom);
                 event.getMember().deafen(false).queue();
             }
         }
         catch (IllegalStateException e){
-            e.printStackTrace();
-            messageChannel.sendMessage(e.getMessage()).queue();
+            logger.warn(e.getMessage(),e);
+            messageChannel.sendMessage("error occurred").queue();
         }
     }
 }
--- src/main/java/TalkingPillow.java
+++ src/main/java/TalkingPillow.java
@@ -1,25 +1,26 @@
 
-import Listeners.MassageListener;
+import Listeners.MessageListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
-import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class TalkingPillow {
     private static final Logger logger = LoggerFactory.getLogger(TalkingPillow.class);
-
     private static final PropertiesConfiguration config = new PropertiesConfiguration();
     public static void main(String[] arguments) throws Exception
     {
         config.load("application.properties");
+        logger.debug("properties loaded");
+
         String botToken = config.getString("bot_token");
         JDA api = JDABuilder.create(botToken, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_BANS,
                         GatewayIntent.SCHEDULED_EVENTS)
-                .addEventListeners(new MassageListener())
+                .addEventListeners(new MessageListener())
                 .build();
+        logger.debug("bot created");
 
 
     }
